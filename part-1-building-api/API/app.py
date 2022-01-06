1b3f96680b9cb060580025a3874fa51dd36d4c4a
KattsonBastos <kattsonbastos@gmail.com>
2022-01-06 19:59:44 -0300

    changing endpoint in order to fit REST pattern

--- part-1-building-api/API/app.py
+++ part-1-building-api/API/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 api = Api(app)
 
-api.add_resource(Classification, '/cvd_classification')
+api.add_resource(Classification, '/cvd-classification')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port= 8000)
